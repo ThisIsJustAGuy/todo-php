5e046560aab8dc76329a1e01d9c6dbe7ebb931ae
ThisIsJustAGuy <bbalint0404@gmail.com>
2024-05-27 12:52:40 +0200

    help

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,6 @@ use App\Http\Controllers\TodosController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::resource('todos', TodosController::class);
@@ -28,3 +16,9 @@ Route::get('logout', function() {
 });
 Auth::routes();
 
+//php artisan make:model BookCategory -crR
+
+//composer require laravel/ui
+//php artisan ui bootstrap --auth
+//npm run build
+//chown -R www-data:www-data /var/www/storage
